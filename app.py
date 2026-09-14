1987e8f5abc8b582285a0298a12d16e699912ad0
praneeth1093 <praneethvakamullu1093@gmail.com>
2026-09-14 16:41:35 +0530

    Remove hardcoded database credentials

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ app.secret_key = "studentpg123"
 UPLOAD_FOLDER = "uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Praneeth@1093'
-app.config['MYSQL_DB'] = 'student_pg'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
